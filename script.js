fd3d8091a99c71ba1deeae873388b4d28339c714
Beslan <beslandudaev8@gmail.com>
2024-12-15 23:14:13 +0300

    verstka

---
+++ script.js
@@ -0,0 +1,37 @@
+var result = document.querySelector('.result');
+var number = document.querySelectorAll('.number');
+var btn = document.getElementsByTagName('button');
+
+btn.forEach(btn => {
+    btn.addEventListener('click', function() {
+    result.textContent = btn;
+    })
+})
+
+function sum (number) {
+
+}
+
+function subtract (number) {
+
+}
+
+function multiply (number) {
+
+}
+
+function division (number) {
+
+}
+
+function operate (num1, num2, operator) {
+    
+}
+
+
+
+
+
+
+//return text.content = const
+// text.content = '';
