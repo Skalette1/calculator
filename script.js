87cb9c2defe7c023dfad439bdce64e286b85ab2d
Beslan <beslandudaev8@gmail.com>
2024-12-17 16:09:34 +0300

    clickcount

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ var result = document.querySelector('.result');
 var btn = document.querySelectorAll('.btn');
 var del = document.getElementById('delete');
 let tochka = document.getElementsByClassName('desyt');
+let clickCount = 0;
+const maxCount = 10;
 
     btn.forEach(element => {
     element.addEventListener('click', () => {
-    if (parseInt(result.textContent + element.textContent) <= 12) {
-    result.textContent += element.textContent
-    }else {
-        result.textContent = "Хьийз ма е и калькулятор"
+    if(clickCount <= maxCount) {
+        if(element.textContent === "." && result.textContent.includes('.')) {
+            return
+        }
+        result.textContent += element.textContent;
+        clickCount ++
+    }
+    else {
+        btn.disabled = true
     }
     })
 });
 
-
-
-
 del.addEventListener('click', () => {
     result.textContent = ''
+    clickCount = 0
 })
 
 
@@ -50,8 +55,18 @@ function operate (num1, num2, operator) {
 //return text.content = const
 // text.content = '';
 
-if (parseInt(result.textContent + element.textContent) <= 12) {
-    result.textContent += element.textContent
-    }else {
-        result.textContent = "Хьийз ма е и калькулятор"
-    }
+// if (parseInt(result.textContent + element.textContent) <= 12) {
+//     result.textContent += element.textContent
+//     }else {
+//         result.textContent = "Хьийз ма е и калькулятор"
+//     }
+//     clickCount ++
+//     if(clickCount === maxCount) {
+//         btn.disabled = true;
+//         result.textContent += ''
+//     }
+
+
+// if(result.textContent <=10) {
+//     alert = "ERROR"
+// }
