126368f5367489e3670ec1ea9ac2d7b3a4fc15ae
Beslan <beslandudaev8@gmail.com>
2024-12-19 00:00:54 +0300

    NaN

--- script.js
+++ script.js
@@ -5,58 +5,75 @@ var plus = document.querySelector('.plus');
 var znak = document.querySelectorAll('.plus_minus');
 var ravno = document.querySelector('.ravno');
 var tochka = document.querySelectorAll('.desyt');
+var operator = document.querySelectorAll('.znak')
+var number = document.querySelectorAll('.number')
 let clickCount = 0;
-const maxCount = 14;
-
-function sum (btn) {
-    let sumBtn = btn + btn;
-    return sumBtn;
-}
-
-plus.addEventListener('click', () => {
-    let sumResult = sumBtn;
-})
-
-function operate () {
-    ravno.addEventListener('click', () => {
-    result.textContent = sumResult.textContent
-    })
-}
+const maxCount = 12;
+var summa = 0
 
     btn.forEach(element => {
     element.addEventListener('click', () => {
     if(clickCount <= maxCount) {
      if(element.textContent === "." && result.textContent.includes('.')) {
-            return
         }
-        result.textContent += element.textContent;
+        summa += parseInt(element.textContent);
+        result.value += element.textContent;
         clickCount ++
     }
     else {
         btn.disabled = true
-     }
+    }
   })
 });
 
-
 del.addEventListener('click', () => {
-    result.textContent = '';
+    result.value = ""
     clickCount = 0
+    summa = 0
+    result.style.color = 'black'
+})
+
+function add (x, y) {
+    return x + y
+}
+
+function subtract (x, y) {
+    return x - y
+}
+
+function multiply (x, y) {
+    return x * y
+}
+
+function division (x, y) {
+    return x / y
+}
+
+function operate (x, y, operator) {
+        if (operator === '+') {
+            return add
+        }
+        else if (operator === '-') {
+            return subtract
+        }
+        else if (operator === '*') {
+            return multiply
+        }
+        else if (operator === '/') {
+            return division
+        }
+        else {
+            return 'ERROR'
+        }
+    }
+
+    ravno.addEventListener('click', () => {
+        result.value += parseFloat(summa);
+        result.style.cssText = 'color: #97680a'
 })
 
 
-
-function subtract (number) {
-
-}
-
-function multiply (number) {
-
-}
-
-function division (number) {
-
-}
+ 
 
 
 // function ChangeZnak(znak, btn) {
